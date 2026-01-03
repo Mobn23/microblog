7c8c63f8e9a3ae434272501bee6d625e79d3cb13
Mobn23 <mobn23@student.bth.se>
2026-01-03 03:43:39 +0100

    fix(version)
    Fixes version unit tests.

--- tests/unit/models/test_versions.py
+++ tests/unit/models/test_versions.py
@@ -12,7 +12,7 @@ def test_version_route(client, monkeypatch):
     :param client: Description
     :param monkeypatch: Description
     """
-    monkeypatch.setenv("APP_VERSION", "v9.9.9")
+    monkeypatch.setenv("MICROBLOG_VERSION", "v9.9.9")
     res = client.get("/version")
     assert res.status_code == 200
     assert res.get_json()["version"] == "v9.9.9"
